e63d795855d1c95a7abd2625830fab1469b0fa09
Rajanmeet Kaur <115046368+MeetWaraich@users.noreply.github.com>
2022-12-05 19:07:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rajanmeet_c0868153_mt/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rajanmeet_c0868153_mt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
